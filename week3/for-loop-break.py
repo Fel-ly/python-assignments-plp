46f4462d537a3889577131d6b81b5e4eaeaccbca
Fel-ly <kiokofpm@gmail.com>
2024-03-11 10:33:12 +0300

    using break in a for loop

---
+++ week3/for-loop-break.py
@@ -0,0 +1,9 @@
+colors = ["blue","green","white","Yellow","brown", "cream"]
+color_I_want = "white"
+
+for color in colors:
+
+    if color == color_I_want:
+        print("They have the color I want!")
+        break
+    print(color)
